09b441bd911af63dd7e14175070a2344a4bc1324
Nate McMaster <nate.mcmaster@microsoft.com>
2016-11-29 10:51:12 -0800

    Update bootstrapper to use dotnet.exe from DOTNET_HOME or PATH if it matches expected version

--- build.ps1
+++ build.ps1
@@ -3,21 +3,36 @@ $ErrorActionPreference='Stop'
 cd $PSScriptRoot
 
 $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-$env:DOTNET_HOME = "$PSScriptRoot/.dotnet"
-$env:PATH += $env:DOTNET_HOME
-mkdir $env:DOTNET_HOME -ErrorAction Ignore | Out-Null
 
-$channel=($(sls 'channel' $PSScriptRoot/cli.yml | select -exp line) -split ': ')[1]
-$env:DotnetCliVersion=($(sls 'version' $PSScriptRoot/cli.yml | select -exp line) -split ': ')[1]
-
-if ( !(Test-Path $env:DOTNET_HOME/dotnet.exe) -or "$(& $env:DOTNET_HOME/dotnet.exe --version)" -ne $env:DotnetCliVersion) {
+function install_dotnet {
+    mkdir $env:DOTNET_HOME -ErrorAction Ignore | Out-Null
     rm -Recurse -Force $env:DOTNET_HOME/sdk -ErrorAction Ignore
-    $target = "$env:DOTNET_HOME/dotnet-install.ps1"
-    if (!(test-path $target)) {
-        Invoke-WebRequest https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1 -OutFile $target
-    }
-    & $env:DOTNET_HOME/dotnet-install.ps1 -InstallDir $env:DOTNET_HOME -Version $env:DotnetCliVersion
+    $channel=($(sls 'channel' $PSScriptRoot/cli.yml | select -exp line) -split ': ')[1]
+    Invoke-WebRequest https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.ps1 -OutFile $env:DOTNET_HOME/dotnet-install.ps1
+    & $env:DOTNET_HOME/dotnet-install.ps1 -InstallDir $env:DOTNET_HOME -Version $env:DotnetCliVersion -Channel $channel
+    $env:PATH = "${env:DOTNET_HOME};${env:PATH}"
 }
+
+function ensure_dotnet {
+    $env:DotnetCliVersion = ($(sls 'version' $PSScriptRoot/cli.yml | select -exp line) -split ': ')[1]
+    if (Test-Path $env:DOTNET_HOME/dotnet.exe -and "$(& $env:DOTNET_HOME/dotnet.exe --version)" -eq $env:DotnetCliVersion) {
+        return
+    }
+    $globalDotnet = Get-Command dotnet
+    if (Test-Path $globalDotnet.Path) {
+        $env:DOTNET_HOME = Split-Path $globalDotnet.Path -Parent
+    } else {
+        $env:DOTNET_HOME = "$PSScriptRoot/.dotnet"
+        if ( !(Test-Path $env:DOTNET_HOME/dotnet.exe) -or "$(& $env:DOTNET_HOME/dotnet.exe --version)" -ne $env:DotnetCliVersion) {
+            install_dotnet
+        }
+    }
+}
+
+# Main
+
+ensure_dotnet
+Write-Host -ForegroundColor Gray "Using $env:DOTNET_HOME/dotnet.exe"
 
 & $env:DOTNET_HOME/dotnet.exe restore build.xml
 if ($LASTEXITCODE -ne 0) {
--- build.sh
+++ build.sh
@@ -5,18 +5,35 @@ cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $cwd > /dev/null
 
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
-export DOTNET_HOME="$cwd/.dotnet"
-export PATH="$DOTNET_HOME:$PATH"
-mkdir -p $DOTNET_HOME
 
-channel=$(cat $cwd/cli.yml | grep 'channel' | awk '{print $2}')
-export DotnetCliVersion=$(cat $cwd/cli.yml | grep 'version' | awk '{print $2}')
-
-if test ! -x $DOTNET_HOME/dotnet || test "$($DOTNET_HOME/dotnet --version)" != $DotnetCliVersion ; then
-    rm -rf $DOTNET_HOME/sdk # clear out potentially bad version of CLI
+install_dotnet() {
+    mkdir -p $DOTNET_HOME
+    rm -rf $DOTNET_HOME/sdk >/dev/null # clear out potentially bad version of CLI
+    channel=$(cat $cwd/cli.yml | grep 'channel' | awk '{print $2}')
     curl -sSL https://raw.githubusercontent.com/dotnet/cli/rel/1.0.0/scripts/obtain/dotnet-install.sh \
-        | bash -s -- -i $DOTNET_HOME --version $DotnetCliVersion
-fi
+        | bash -s -- -i $DOTNET_HOME --version $DotnetCliVersion --channel $channel
+    export PATH="$DOTNET_HOME:$PATH"
+}
+
+ensure_dotnet() {
+    export DotnetCliVersion=$(cat $cwd/cli.yml | grep 'version' | awk '{print $2}')
+    if test -x $DOTNET_HOME/dotnet && test "$($DOTNET_HOME/dotnet --version)" = $DotnetCliVersion ; then
+        return
+    elif which dotnet >/dev/null && "$(dotnet --version)" -eq $DotnetCliVersion; then
+        local _dotnet="$(which dotnet)"
+        export DOTNET_HOME="$(dirname $_dotnet)"
+    else
+        export DOTNET_HOME="$cwd/.dotnet"
+        if test ! -x $DOTNET_HOME/dotnet || test "$($DOTNET_HOME/dotnet --version)" != $DotnetCliVersion; then
+            install_dotnet
+        fi
+    fi
+}
+
+# Main
+
+ensure_dotnet
+printf "\033[90mUsing $DOTNET_HOME/dotnet\033[0m\n"
 
 $DOTNET_HOME/dotnet restore build.xml
 $DOTNET_HOME/dotnet msbuild build.xml /nologo /v:m /m "$@"
